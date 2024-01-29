73b2d610b2ce990be9aae2641ae028b70fc36250
dshtryhel <56920698+dshtryhel@users.noreply.github.com>
2024-01-29 11:18:59 +0100

    Implemented unit tests

--- GymondoTestTests/ExerciseDetailsViewModelUnitTests.swift
+++ GymondoTestTests/ExerciseDetailsViewModelUnitTests.swift
@@ -6,30 +6,75 @@
 //
 
 import XCTest
+@testable import GymondoTest
 
 final class ExerciseDetailsViewModelUnitTests: XCTestCase {
+    
+    private var viewModel: ExerciseDetailsViewModelProtocol!
+    private let exerciseBaseInfo = ExerciseBaseInfo(
+        id: 1,
+        images: [ExerciseImage(image: URL(string: "gymondo.com")!,
+        isMain: true)],
+        exercises: [Exercise(id: 1, name: "Title", description: "Text")]
+    )
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        
+        viewModel = ExerciseDetailsViewModel(exerciseBaseInfo: exerciseBaseInfo)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        viewModel = nil
+        
+        super.tearDown()
+    }
+    
+    func testEmitShowExerciseTitle() {
+        let expectation = self.expectation(description: "Wait for show correct title exercise")
+        
+        viewModel.output.exerciseName.drive(onNext: { [weak self] value in
+            XCTAssertEqual(value, self?.exerciseBaseInfo.exercises.first?.name)
+            expectation.fulfill()
+        }).disposed(by: viewModel.bag)
+        
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func testInitEmitShowExerciseCellsEvent() {
+        let expectation = self.expectation(description: "Wait for successful show exercise cells")
+        
+        viewModel.output.showExerciseCells.drive(onNext: { [weak self] value in
+            XCTAssertEqual(value.first?.name, self?.exerciseBaseInfo.exercises.first?.name)
+            expectation.fulfill()
+        }).disposed(by: viewModel.bag)
+        
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func testInitEmitShowVariationCellsEvent() {
+        let expectation = self.expectation(description: "Wait for successful show variation cells")
+        
+        viewModel.output.showVariationCells.drive(onNext: { [weak self] value in
+            XCTAssertEqual(value.first?.image?.absoluteString, self?.exerciseBaseInfo.images.first?.image.absoluteString)
+            expectation.fulfill()
+        }).disposed(by: viewModel.bag)
+        
+        wait(for: [expectation], timeout: 1)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testDidTapExerciseDescription() {
+        let expectation = self.expectation(description: "Exercise description did tap")
+        
+        viewModel.output.didTapExerciseDescription.drive(onNext: { [weak self] value in
+            XCTAssertEqual(value?.name, self?.exerciseBaseInfo.exercises.first?.name)
+            XCTAssertEqual(value?.description, self?.exerciseBaseInfo.exercises.first?.description)
+            expectation.fulfill()
+        }).disposed(by: viewModel.bag)
+        
+        viewModel.input.didSelectExerciseDescription.onNext(0)
+        
+        wait(for: [expectation], timeout: 1)
     }
 
 }
--- GymondoTestTests/ExerciseListViewModelUnitTests.swift
+++ GymondoTestTests/ExerciseListViewModelUnitTests.swift
@@ -6,30 +6,97 @@
 //
 
 import XCTest
+import RxSwift
+@testable import GymondoTest
 
 final class ExerciseListViewModelUnitTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    private var viewModel: ExerciseListViewModelProtocol!
+    private var serviceFake: FakeExerciseListService!
+    private let exerciseBaseInfo = ExerciseBaseInfo(
+        id: 1,
+        images: [ExerciseImage(image: URL(string: "gymondo.com")!,
+        isMain: true)],
+        exercises: [Exercise(id: 1, name: "Title", description: "Text")]
+    )
+    private let error = NSError(domain: "FakeErrorDomain", code: 123, userInfo: nil)
+    
+    override func setUp() {
+        super.setUp()
+        serviceFake = FakeExerciseListService()
+        viewModel = ExerciseListViewModel(service: serviceFake)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        viewModel = nil
+        
+        super.tearDown()
+    }
+    
+    func testGetListWithSuccessfulResponse() {
+        let expectation = self.expectation(description: "Wait for successful response")
+        serviceFake.fakeData = [exerciseBaseInfo]
+        
+        viewModel.output.showCells.drive(onNext: { [weak self] value in
+            XCTAssertEqual(value.first?.name, self?.exerciseBaseInfo.exercises.first?.name)
+            XCTAssertEqual(value.first?.image?.absoluteString, self?.exerciseBaseInfo.images.first?.image.absoluteString)
+            expectation.fulfill()
+        }).disposed(by: viewModel.bag)
+        
+        
+        viewModel.input.getList.onNext(())
+        
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func testGetListWithFailedResponse() {
+        let expectation = self.expectation(description: "Wait for failed response")
+        serviceFake.error = error
+        
+        viewModel.output.error.drive(onNext: { error in
+            expectation.fulfill()
+        }).disposed(by: viewModel.bag)
+        
+        
+        viewModel.input.getList.onNext(())
+        
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func testDidSelectExercise() {
+        let expectation = self.expectation(description: "Exercise did tap")
+        serviceFake.fakeData = [exerciseBaseInfo]
+        
+        viewModel.output.didTapExerciseDetails.drive(onNext: { [weak self] value in
+            XCTAssertEqual(value?.exercises.first?.name, self?.exerciseBaseInfo.exercises.first?.name)
+            XCTAssertEqual(value?.images.first?.image.absoluteString, self?.exerciseBaseInfo.images.first?.image.absoluteString)
+            expectation.fulfill()
+        }).disposed(by: viewModel.bag)
+        
+        viewModel.input.getList.onNext(())
+        
+        viewModel.input.didSelectExercise.onNext(0)
+        
+        wait(for: [expectation], timeout: 1)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+}
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+final class FakeExerciseListService: ExerciseListServiceProtocol {
+    
+    let manager = NetworkManager()
+    let bag = DisposeBag()
+    
+    var fakeData: [ExerciseBaseInfo] = []
+    var error: Error?
+    
+    func getExerciseBaseInfo() -> Observable<[ExerciseBaseInfo]> {
+        if let error = error {
+            return Observable<[ExerciseBaseInfo]>.error(error)
+        } else {
+            return Observable.just(fakeData)
         }
+        
     }
-
+    
 }
